c87143b200554fdc1cb190cdc41e8b6b638633b4
Mike MacFerrin <michael.macferrin@colorado.edu>
2019-03-06 15:45:48 -0700

    * Updated code for optional test_n parameters (default 50). * Wrote a utility function "confidence_bounds" that combines "lin_fit", "conf_calc", and "ylines_calc" to do all that in one function call. * Put main code in a "if __name__ == "__main__":" block so that this code can be imported from other python modules without executing the example.

--- CIs_LinearRegression.py
+++ CIs_LinearRegression.py
@@ -56,7 +56,7 @@ def lin_fit(x,y):
 
 # ----------------------------------------------------------------------------
 
-def conf_calc(x, y_err, c_limit=0.975):
+def conf_calc(x, y_err, c_limit=0.975, test_n=50):
     '''
     Calculates confidence interval of regression between x and y
 
@@ -68,9 +68,13 @@ def conf_calc(x, y_err, c_limit=0.975):
              of the critical value in the t-statistic table
              eg: for a 2 tailed 95% confidence interval (the default)
                     c_limit = 0.975
+    test_n:  (optional) integer representing numer of equally-spaced
+             x-points to predict in the range min(x) to max(x)
 
     Returns
     -------
+    p_x:   1D array, length "test_n", with values spread evenly between
+           min(x) and max(x)
     confs: 1D numpy array of predicted y values for x inputs
 
     '''
@@ -82,7 +86,7 @@ def conf_calc(x, y_err, c_limit=0.975):
     s_err = np.sum(np.power(y_err,2))	# sum of the squares of the residuals
 
     # create series of new test x-values to predict for
-    p_x = np.linspace(np.min(x),np.max(x),50)
+    p_x = np.linspace(np.min(x),np.max(x),test_n)
 
     confs = tstat * np.sqrt((s_err/(n-2))*(1.0/n + (np.power((p_x-mean_x),2)/
 			((np.sum(np.power(x,2)))-n*(np.power(mean_x,2))))))
@@ -99,7 +103,7 @@ def ylines_calc(p_x, confs, fit):
     ----------
     p_x:   1D array with values spread evenly between min(x) and max(x)
     confs: 1D array with confidence values for each value of p_x
-    fit:
+    fit:   Predicted x -> y function (typically derived from np.poly1d)
 
     Returns
     -------
@@ -127,39 +131,79 @@ def plot_linreg_CIs(x, y, p_x, p_y, lower, upper):
     plt.title('Linear regression and confidence limits')
 
     # plot sample data
-    plt.plot(x,y,'bo',label='Sample observations')
+    plt.plot(x, y, 'bo', label='Sample observations')
 
     # plot line of best fit
-    plt.plot(p_x,p_y,'r-',label='Regression line')
+    plt.plot(p_x, p_y, 'r-', label='Regression line')
 
     # plot confidence limits
-    plt.plot(p_x,lower,'b--',label='Lower confidence limit (95%)')
-    plt.plot(p_x,upper,'b--',label='Upper confidence limit (95%)')
+    plt.plot(p_x, lower, 'b--', label='Lower confidence limit (95%)')
+    plt.plot(p_x, upper, 'b--', label='Upper confidence limit (95%)')
 
     # show the plot
     plt.show()
 
+
+def confidence_bounds(x, y, c_limit=0.975, test_n=50):
+    '''
+    Calculates the predicted (trend) line, and lower & upper confidene bounds
+    from original x,y measurements
+
+    Parameters
+    ----------
+    x: 1D numpy array
+    y: 1D numpy array (same length as x)
+    c_limit: (optional) float number representing the area to the left
+             of the critical value in the t-statistic table
+             eg: for a 2 tailed 95% confidence interval (the default)
+                    c_limit = 0.975
+    test_n:  (optional) integer representing numer of equally-spaced
+             x-points to predict in the range min(x) to max(x)
+
+    Returns
+    -------
+    p_x:    1D array with values spread evenly between min(x) and max(x)
+            (length test_n)
+    p_y:    1D array with values corresponding to fit line (for p_x values)
+            (length test_n)
+    upper:  1D array, values corresponding to upper confidence limit line
+    lower:  1D array, values corresponding to lower confidence limit line
+
+    '''
+
+    # Fit x to y
+    p, y_err = lin_fit(x ,y)
+
+    # Calculate confidence intervals
+    p_x, confs = conf_calc(x, y_err, c_limit=c_limit, test_n=test_n)
+
+    # Calculate the lines for plotting:
+    # The fit line, and lower and upper confidence bounds
+    p_y, lower, upper = ylines_calc(p_x, confs, p)
+
+    return p_x, p_y, lower, upper
+
 # ============================================================================
 
-# ====== MAIN CODE ===========================================================
+# ====== MAIN CODE, SAMPLE USE ===============================================
+if __name__ == "__main__":
+    # Define example data
+    x = np.linspace(1,15,50)
+    y = x * 4 + 2.5
+    x = x + np.random.random_sample(size=x.shape) * 20
+    y = y + np.random.random_sample(size=x.shape) * 20
 
-# Define example data
-x = np.linspace(1,15,50)
-y = x * 4 + 2.5
-x = x + np.random.random_sample(size=x.shape) * 20
-y = y + np.random.random_sample(size=x.shape) * 20
+    # Fit x to y
+    p, y_err = lin_fit(x, y)
 
-# Fit x to y
-p, y_err = lin_fit(x,y)
+    # Calculate confidence intervals
+    p_x, confs = conf_calc(x, y_err, 0.975)
 
-# Calculate confidence intervals
-p_x, confs = conf_calc(x, y_err, 0.975)
+    # Calculate the lines for plotting:
+    # The fit line, and lower and upper confidence bounds
+    p_y, lower, upper = ylines_calc(p_x, confs, p)
 
-# Calculate the lines for plotting:
-# The fit line, and lower and upper confidence bounds
-p_y, lower, upper = ylines_calc(p_x, confs, p)
-
-# Plot these lines
-plot_linreg_CIs(x, y, p_x, p_y, lower, upper)
+    # Plot these lines
+    plot_linreg_CIs(x, y, p_x, p_y, lower, upper)
 
 # ============================================================================
