9ad0048e6f1a837abcd282686188e11a9c7c263a
Mike MacFerrin <michael.macferrin@colorado.edu>
2019-03-06 14:01:27 -0700

    Fixed bug in Line 112: p_y = p(p_x) ...to... p_y = fit(p_x)

--- CIs_LinearRegression.py
+++ CIs_LinearRegression.py
@@ -6,12 +6,12 @@ Calculating confidence intervals for a linear regression
 Heavily inspired (read: copied) from:
     linfit.py - example of confidence limit calculation for linear
                 regression fitting.
-    
+
     http://tomholderness.wordpress.com/2013/01/10/confidence_intervals/
 
-# References: 
+# References:
 # - Statistics in Geography by David Ebdon (ISBN: 978-0631136880)
-# - Reliability Engineering Resource Website: 
+# - Reliability Engineering Resource Website:
 # - http://www.weibull.com/DOEWeb/confidence_intervals_in_simple_linear_regression.htm
 # - University of Glascow, Department of Statistics:
 # - http://www.stats.gla.ac.uk/steps/glossary/confidence_intervals.html#conflim
@@ -19,7 +19,7 @@ Heavily inspired (read: copied) from:
 By Kirstie Whitaker, on 27th September 2013
     Contact:  kw401@cam.ac.uk
     GitHubID: HappyPenguin
-    
+
 '''
 
 # ====== IMPORTS =============================================================
@@ -32,26 +32,26 @@ from scipy.stats import t
 def lin_fit(x,y):
     '''
     Predicts the values for a best fit between numpy arrays x and y
-    
+
     Parameters
     ----------
     x: 1D numpy array
     y: 1D numpy array (same length as x)
-    
+
     Returns
     -------
     p:     parameters for linear fit of x to y
-    y_err: 1D array of difference between y and fit values    
+    y_err: 1D array of difference between y and fit values
                (same length as x)
-    
+
     '''
-    
+
     z = np.polyfit(x,y,1)
     p = np.poly1d(z)
     fit = p(x)
 
     y_err = y - fit
-    
+
     return p, y_err
 
 # ----------------------------------------------------------------------------
@@ -59,7 +59,7 @@ def lin_fit(x,y):
 def conf_calc(x, y_err, c_limit=0.975):
     '''
     Calculates confidence interval of regression between x and y
-    
+
     Parameters
     ----------
     x:       1D numpy array
@@ -72,7 +72,7 @@ def conf_calc(x, y_err, c_limit=0.975):
     Returns
     -------
     confs: 1D numpy array of predicted y values for x inputs
-    
+
     '''
     # Define the variables you need
     # to calculate the confidence interval
@@ -88,37 +88,37 @@ def conf_calc(x, y_err, c_limit=0.975):
 			((np.sum(np.power(x,2)))-n*(np.power(mean_x,2))))))
 
     return p_x, confs
-    
+
 # ----------------------------------------------------------------------------
 
 def ylines_calc(p_x, confs, fit):
     '''
     Calculates the three lines that will be plotted
-    
+
     Parameters
     ----------
     p_x:   1D array with values spread evenly between min(x) and max(x)
     confs: 1D array with confidence values for each value of p_x
-    fit:   
-    
+    fit:
+
     Returns
     -------
     p_y:    1D array with values corresponding to fit line (for p_x values)
     upper:  1D array, values corresponding to upper confidence limit line
     lower:  1D array, values corresponding to lower confidence limit line
-    
+
     '''
     # now predict y based on test x-values
-    p_y = p(p_x)
-    
+    p_y = fit(p_x)
+
     # get lower and upper confidence limits based on predicted y and confidence intervals
     lower = p_y - abs(confs)
     upper = p_y + abs(confs)
 
     return p_y, lower, upper
-   
+
 # ----------------------------------------------------------------------------
- 
+
 def plot_linreg_CIs(x, y, p_x, p_y, lower, upper):
 
     # set-up the plot
@@ -135,7 +135,7 @@ def plot_linreg_CIs(x, y, p_x, p_y, lower, upper):
     # plot confidence limits
     plt.plot(p_x,lower,'b--',label='Lower confidence limit (95%)')
     plt.plot(p_x,upper,'b--',label='Upper confidence limit (95%)')
-    
+
     # show the plot
     plt.show()
 
